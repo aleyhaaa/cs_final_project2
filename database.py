5cb16f8d62ccd98839365244035476103d539507
aleyhaaa <aleyhaalelawi@gmail.com>
2025-03-24 15:37:11 -0500

    Created a database.py file

---
+++ database.py
@@ -0,0 +1,37 @@
+"""
+This file will control all aspects of communicating with the SQLite3 database
+"""
+
+# Initialize a database - SQLite3
+
+def initialize_database():
+    """
+    Initialize database to store data
+    """
+    pass
+
+# Create book table
+def create_book_table():
+    """
+    This function will create a book table based off of the following
+
+    - books
+        - pid (primary id) - a unique number
+        - title:str
+        - author_id:int
+        - read_book:bool
+        - comments:str
+        - isbn:str
+
+    """
+    pass
+# Create an author table
+def create_author_table():
+    """
+    This function will create an author table based off of the following 
+    - author
+        - pid
+        - first_name:str
+        - last_name:str
+    """
+    pass
